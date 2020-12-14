a09c4cfb6bc0af081559520445c65cd12688853b
Abby Prince <ab>
2020-12-14 12:55:44 -0500

    mock /sighting endpoint

--- index.js
+++ index.js
@@ -8,9 +8,13 @@ app.use(express.static(path.join(__dirname + '/public')));
 
 // homepage
 app.get('/', (req, res) => {
-res.sendFile(path.join(__dirname + '/public/index.html'));
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 app.listen(55555, () => {
-console.log("Lionfish server listening on port 55555");
+    console.log("Lionfish server listening on port 55555");
+});
+
+app.get('/sighting*', async function (req, res) {
+    res.json([{ name: "Bob" }]);
 });
