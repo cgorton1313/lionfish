1caf4f688151a73509e1d562e33a8d4cf2441684
Abby Prince <ab>
2020-12-16 10:16:48 -0500

    better sightings mock

--- lionfishData.js
+++ lionfishData.js
@@ -1,7 +1,13 @@
-function getData() {
-    return "Bob";
+var sightingsInfo = [
+    {latitude:42.4072, longitude:71.3824, id:1},
+    {latitude:43.1939, longitude:71.5724, id:2},
+    {latitude:41.6032, longitude:73.0877, id:3}
+]
+
+function getSightings() {
+    return sightingsInfo;
 }
 
 module.exports = {
-    getData
+    getSightings
 }
