f3bf48680b35f60ea2fb280ea0f0d3db607a0c42
Elaina Kreher <elainakreher@Elainas-MacBook.fios-router.home>
2021-01-04 13:46:36 -0500

    trying to get depth to work

--- data/depth.js
+++ data/depth.js
@@ -1,21 +1,23 @@
 const fs = require('fs');
 const csv = require('csvtojson');
-const csvFilePath = 'data/NAS-Data-Download.csv';
+const csvFilePath = 'puller/NAS-Data-Download-11-02-2020-74078/NAS-Data-Download.csv';
 
 csv().fromFile(csvFilePath).then(function (data) {
 doSomething(data);
-}); 
+});
 
 function doSomething(data) {
+  console.log(data);
     // for (i = 0; i < data.length; i++) {
     //     if (data.inculdes('depth')) {
-    //         console.log(data);
+    //         console.log('there is depth');
     //     }
     // }
 
     // for (i = 0; i < data.day.length; i++) {
     //     x += data.day[i] + "<br>";
     //   }
-
-      console.log(data);
+  // for (var i=0; i<data.length; i++) {
+  //     if (data[i].match(str)) return i;
+  // }
 }
