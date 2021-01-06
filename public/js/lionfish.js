e7742fb0d0cd87edfd3a52e8a2bf47218a14b567
Molly O'Handley <mollyohandley@Mollys-MacBook-Air.local>
2021-01-06 09:39:02 -0500

    add marker

--- public/js/lionfish.js
+++ public/js/lionfish.js
@@ -6,39 +6,39 @@ function getUserPosition() {
     if (navigator.geolocation) {
         return navigator.geolocation.getCurrentPosition(loadChart, showError);
     } else {
-    showError();
+        showError();
     }
-    }
+}
 
-    function loadChart(position, zoomLevel = 13) { 
-        // use zoom level 10 if geo is on
-        let userLat = position.coords.latitude;
-        let userLon = position.coords.longitude;
-         chart = L.map('chart', {
-            minZoom: 0,
-            maxZoom: 20
-        }).setView([userLat, userLon], zoomLevel);
-        L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/{z}/{y}/{x}', {
-        }).addTo(chart);
-        }
+function loadChart(position, zoomLevel = 13) {
+    // use zoom level 10 if geo is on
+    let userLat = position.coords.latitude;
+    let userLon = position.coords.longitude;
+    chart = L.map('chart', {
+        minZoom: 0,
+        maxZoom: 20
+    }).setView([userLat, userLon], zoomLevel);
+    L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/{z}/{y}/{x}', {
+    }).addTo(chart);
+    L.marker([userLat, userLon]).addTo(chart);
+}
 
-    function showError(error) {
-            switch (error.code) {
-            case error.PERMISSION_DENIED:
+function showError(error) {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
             console.log("User denied the request for Geolocation.");
             break;
-            case error.POSITION_UNAVAILABLE:
+        case error.POSITION_UNAVAILABLE:
             console.log("Location information is unavailable.");
             break;
-            case error.TIMEOUT:
+        case error.TIMEOUT:
             console.log("The request to get user location timed out.");
             break;
-            case error.UNKNOWN_ERROR:
+        case error.UNKNOWN_ERROR:
             console.log("An unknown error occurred.");
             break;
-            }
-            let position = { coords: { latitude: 27, longitude: -80 } };
-            loadChart(position, 10); // if user doesn't have or allow geo, show default position and zoom
-            }
-            
-            
+    }
+    let position = { coords: { latitude: 27, longitude: -80 } };
+    loadChart(position, 10); // if user doesn't have or allow geo, show default position and zoom
+}
+
