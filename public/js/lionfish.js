766f5052210a42ad72074c26244ffea960bc220f
Craig Gorton <cgorton@penguinhall.org>
2021-01-29 13:32:20 -0500

    fix user icon

--- public/js/lionfish.js
+++ public/js/lionfish.js
@@ -1,8 +1,16 @@
 let chart;
 
-let userPositionIcon = L.icon({
-    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+// Icons
+var LeafIcon = L.Icon.extend({
+    options: {
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    }
 });
+let userPositionIcon = new LeafIcon({ iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png' });
 
 getUserPosition();
 
